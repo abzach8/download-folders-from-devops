23b223c7f067f8d284ff09f638a2d8733d4e6f0f
Abbia Zacharia <58935958+abzach8@users.noreply.github.com>
2022-11-15 16:36:27 +0530

    Update DevOps-Repo-Download.ps1

--- DevOps-Repo-Download.ps1
+++ DevOps-Repo-Download.ps1
@@ -1,11 +1,32 @@
-﻿# PowerShell script to download folders and all its contents from Azure Devops repos
+<#
 
-function RepoDownload ($devopspath)
+Name: DevOps-Repo-Download.ps1
+Version: 2
+Author: Abbia Zacharia
+
+Purpose: 
+Download folders and all its contents from Azure Devops repository 
+
+Description:
+
+Version Control:
+
+Prerequisites:
+
+Configurations:
+
+#>
+
+#==================================================================================
+
+function RepoDownload ()
 {
+Write-Host "Example: SPO/Source/Dashboard" -ForegroundColor Blue
+$filePath = Read-Host -Prompt "Enter Devops root path to download from"
 $filePath = "*"+ $devopspath + "/*"
 $urlDownloadItems = "https://dev.azure.com/$organization/$project/_apis/git/repositories/$($repoName)/items?recursionLevel=Full&api-version=5.0"
 $itemPaths = Invoke-RestMethod -Uri $urlDownloadItems -Method Get -ContentType "application/text" -Headers $header
-Write-Host "`nDownload started from $filePath..." -ForegroundColor Yellow
+Write-Host "`Started download from $filePath" -ForegroundColor Yellow
 foreach ($i in $itemPaths.value) 
 {
 if (-not($i.path -like $filePath))
@@ -33,18 +54,32 @@ if (-not($i.path -like $filePath))
 Write-Host "`nFinished downloading from $filePath" -ForegroundColor Green
 }
 
-# Code starts from here
-Write-Host "`nScript started.." -ForegroundColor Yellow
+# Code is executing from here
+Write-Host "`nPowerShell script to download folders and all its contents from ADO repo" -ForegroundColor Yellow
 Write-Host "************************" -ForegroundColor White -BackgroundColor Black
+
+Write-Host "Time to supply ADO details" -ForegroundColor Red
 $organization = Read-Host -Prompt "Enter Devops organization name"
 $project = Read-Host -Prompt "Enter Devops project name"
 $repoName = Read-Host -Prompt "Enter Devops repo name"
-$filePath = Read-Host -Prompt "Enter Devops path to download from"
+
 $localpath = $PSScriptRoot
-$tokenInSecureString = Read-Host "Enter personal access token (PAT)" -AsSecureString
+$tokenInSecureString = Read-Host "Enter personal access token to access $repoName (PAT)" -AsSecureString
 $dummyCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'fakeUser', $tokenInSecureString
 $base64AuthString = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "", $dummyCredential.GetNetworkCredential().Password)))
 $header = @{
     "Authorization" = ("Basic {0}" -f $base64AuthString)
 }
-RepoDownload ($filePath)
+
+RepoDownload
+$confirmation = Read-Host "Do you want to download one more root folder? y/n"
+if ($confirmation -eq 'y') 
+{
+RepoDownload
+}
+else
+{
+exit
+}
+
+#==================================================================================
